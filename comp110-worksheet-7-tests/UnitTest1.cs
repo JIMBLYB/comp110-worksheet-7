90ba5a284ac606e4811ef26f13904c9f39299e62
James <jimblyb2001@gmail.com>
2019-11-28 15:50:45 +0000

    Replaced all code with working test files

--- comp110-worksheet-7-tests/UnitTest1.cs
+++ comp110-worksheet-7-tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using comp110_worksheet_7;
+using comp110_worksheet_7;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -11,7 +11,10 @@ namespace comp110_worksheet_7_tests
 	[TestFixture]
 	public class UnitTest1
 	{
-		const string c_baseDirectory = "comp110-worksheet-7-tests/pixelcarpack_kenney/PNG";
+		string c_baseDirectory
+		{
+			get { return Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", "pixelcarpack_kenney", "PNG"); }
+		}
 
 		[Test]
 		public void TestBaseDirectoryExists()
