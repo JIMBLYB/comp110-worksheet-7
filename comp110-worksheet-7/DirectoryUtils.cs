968ca5d6987ce0d45f84a136920456bf78fddaa4
JamesB <git@jamesberry.co.uk>
2019-12-02 12:25:21 +0000

    Completed tasks

--- comp110-worksheet-7/DirectoryUtils.cs
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -26,27 +26,26 @@ namespace comp110_worksheet_7
 		{
             long directorySize = 0;
             Stack<string> directoriesLeft = new Stack<string>();
-            directoriesLeft.Push(directory);
-            while(directoriesLeft.Count != 0)
+
+            if (IsDirectory(directory))
             {
-                foreach (string newDirectory in Directory.GetDirectories(directoriesLeft.Pop()))
+                directoriesLeft.Push(directory);
+
+                while (directoriesLeft.Count > 0)
                 {
-                    if (Directory.GetDirectories(newDirectory) != null)
+                    string currentDirectory = directoriesLeft.Pop();
+                    string[] newDirectories = Directory.GetDirectories(currentDirectory);
+                    string[] newfiles = Directory.GetFiles(currentDirectory);
+
+                    for (int i = 0; i < newDirectories.Length; i++)
                     {
-                        foreach (string addDirectory in Directory.GetDirectories(newDirectory))
-                        {
-                            Console.WriteLine(addDirectory);
-                            directoriesLeft.Push(addDirectory);
-                        }
+                        directoriesLeft.Push(newDirectories[i]);
                     }
-                    if (Directory.GetFiles(newDirectory) != null)
+
+                    for (int i = 0; i < newfiles.Length; i++)
                     {
-                        foreach(string file in Directory.GetFiles(newDirectory))
-                        {
-                            Console.WriteLine(file);
-                            directorySize += GetFileSize(file);
-                        }
-                    }                  
+                        directorySize += GetFileSize(newfiles[i]);
+                    }
                 }
             }
             return directorySize;
@@ -55,31 +54,174 @@ namespace comp110_worksheet_7
 		// Return the number of files (not counting directories) below the given directory
 		public static int CountFiles(string directory)
 		{
-			throw new NotImplementedException();
-		}
+            int directoryCount = 0;
+            Stack<string> directoriesLeft = new Stack<string>();
 
-		// Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
+            if (IsDirectory(directory))
+            {
+                directoriesLeft.Push(directory);
+
+                while (directoriesLeft.Count > 0)
+                {
+                    string currentDirectory = directoriesLeft.Pop();
+                    string[] newDirectories = Directory.GetDirectories(currentDirectory);
+                    string[] newfiles = Directory.GetFiles(currentDirectory);
+
+                    for (int i = 0; i < newDirectories.Length; i++)
+                    {
+                        directoriesLeft.Push(newDirectories[i]);
+                    }
+
+                    for (int i = 0; i < newfiles.Length; i++)
+                    {
+                        directoryCount++;
+                    }
+                }
+            }
+            return directoryCount;
+        }
+
+		// Return the nesting depth of the given directory. A directory containing only files (no currentDirectories) has a depth of 0.
 		public static int GetDepth(string directory)
 		{
-			throw new NotImplementedException();
-		}
+            int depth = 0;
+            Stack<string> directoriesLeft = new Stack<string>();
+            Stack<string> currentDirectories = new Stack<string>();
+
+            if (IsDirectory(directory))
+            {
+                while (currentDirectories.Count > 0)
+                {
+                    foreach (string files in currentDirectories)
+                    {
+                        string[] newDirectories = Directory.GetDirectories(currentDirectories.Pop());
+
+                        foreach (string directoryPath in newDirectories)
+                        {
+                            directoriesLeft.Push(directoryPath);
+                        }
+                    }
+
+                    if (currentDirectories.Count > 0)
+                    {
+                        currentDirectories.Clear();
+
+                        for (int i = 0; i < currentDirectories.Count; i++)
+                        {
+                            currentDirectories.Push(currentDirectories.Pop());
+                        }
+
+                        currentDirectories.Clear();
+                        depth++;
+                    }
+                }
+            }
+            return depth;
+ 		}
 
 		// Get the path and size (in bytes) of the smallest file below the given directory
 		public static Tuple<string, long> GetSmallestFile(string directory)
 		{
-			throw new NotImplementedException();
-		}
+            Tuple<String, long> smallestFile = new Tuple<string, long>("Directory not found", 1000000000000);
+            Stack<string> subDirectories = new Stack<string>();
+
+            if (IsDirectory(directory))
+            {
+                subDirectories.Push(directory);
+
+                while (subDirectories.Count > 0)
+                {
+                    string currentDirectory = subDirectories.Pop();
+                    string[] newDirectories = Directory.GetDirectories(currentDirectory);
+                    string[] newFiles = Directory.GetFiles(currentDirectory);
+
+                    foreach (string directories in newDirectories)
+                    {
+                        subDirectories.Push(directories);
+                    }
+
+                    foreach (string files in newFiles)
+                    {
+                        long fileSize = GetFileSize(files);
+
+                        if (fileSize < smallestFile.Item2)
+                        {
+                            smallestFile = new Tuple<string, long>(files, fileSize);
+                        }
+                    }
+                }
+            }
+            return smallestFile;
+        }
 
 		// Get the path and size (in bytes) of the largest file below the given directory
 		public static Tuple<string, long> GetLargestFile(string directory)
 		{
-			throw new NotImplementedException();
-		}
+            Tuple<String, long> largestFile = new Tuple<string, long>("Directory not found", 0);
+            Stack<string> subDirectories = new Stack<string>();
+
+            if (IsDirectory(directory))
+            {
+                subDirectories.Push(directory);
+
+                while (subDirectories.Count > 0)
+                {
+                    string currentDirectory = subDirectories.Pop();
+                    string[] newDirectories = Directory.GetDirectories(currentDirectory);
+                    string[] newFiles = Directory.GetFiles(currentDirectory);
+
+                    foreach (string directories in newDirectories)
+                    {
+                        subDirectories.Push(directories);
+                    }
+
+                    foreach (string files in newFiles)
+                    {
+                        long fileSize = GetFileSize(files);
+
+                        if (fileSize > largestFile.Item2)
+                        {
+                            largestFile = new Tuple<string, long>(files, fileSize);
+                        }
+                    }
+                }
+            }
+            return largestFile;
+        }
 
 		// Get all files whose size is equal to the given value (in bytes) below the given directory
 		public static IEnumerable<string> GetFilesOfSize(string directory, long size)
 		{
-			throw new NotImplementedException();
-		}
+            List<string> filesOfSize = new List<string>();
+            Stack<string> subDirectories = new Stack<string>();
+
+            if (IsDirectory(directory))
+            {
+                subDirectories.Push(directory);
+
+                while (subDirectories.Count > 0)
+                {
+                    string currentDirectory = subDirectories.Pop();
+                    string[] newDirectories = Directory.GetDirectories(currentDirectory);
+                    string[] newFiles = Directory.GetFiles(currentDirectory);
+
+                    foreach (string directories in newDirectories)
+                    {
+                        subDirectories.Push(directories);
+                    }
+
+                    foreach (string files in newFiles)
+                    {
+                        long fileSize = GetFileSize(files);
+
+                        if (fileSize == size)
+                        {
+                            filesOfSize.Add(files);
+                        }
+                    }
+                }
+            }
+            return filesOfSize;
+        }
 	}
 }
